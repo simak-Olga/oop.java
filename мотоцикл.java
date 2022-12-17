e009dd5e0da0df322ef7f1c8ff5bc03f248da192
Olga <beauty-90@list.rugit config --global user.email beauty-90@list.ru>
2022-12-17 18:55:15 +0300

    oop1

---
+++ мотоцикл.java
@@ -0,0 +1,19 @@
+public пакет урока1;
+
+Мотоцикл общественного класса расширяет группу транспорта для передвижения Movement {
+    частная внутренняя скорость;
+
+    public int getVolumeLine() {
+        internal combustion engine;
+    }
+
+    общественный пустой setVolumeLine(int VolumeLine) {
+        это. VolumeLine = volumeline;
+    }
+
+    @Перекрыть
+    пустотная нагрузка() {
+        Система. вон. println("мотоцикл едет с пассажирами быстро");
+    }
+
+}
---
+++ транспорт.java
@@ -0,0 +1,16 @@
+пакет урока1;
+
+публичный абстрактный класс Транспорт {
+    private int loadCapacity;
+
+    public int getLoadCapacity() {
+        возврат нагрузкиЕмкость;
+    }
+
+    public void setLoadCapacity(int loadCapacity) {
+        это. loadCapacity = loadCapacity;
+    }
+
+
+    абстрактная пустая нагрузка();
+}
