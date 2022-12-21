692a8d74bbc7089a4f40fa9ca6f6bbc7a1621815
Olga <beauty-90@list.rugit config --global user.email beauty-90@list.ru>
2022-12-21 22:14:24 +0300

    interface

---
+++ interface flying.java
@@ -0,0 +1,21 @@
+пакет урока 1
+public class interface flying {
+
+    public class helicopter расширяет транспорт
+    частный строковый маршрут
+    
+
+    public int getVolumeLine() {
+        internal combustion engine;
+
+        общественный пустой setVolumeLine(int VolumeLine) {
+            это. VolumeLine = volumeline;
+        }
+    
+        @Перекрыть
+        пустотная нагрузка() {
+            Система. вон. println("вертолёт взлетает и перевозит людей");
+        }
+    
+    } 
+}
---
+++ interface travelling.java
@@ -0,0 +1,20 @@
+пакет урока1;
+public class interface travelling {
+    
+    public class sport car расширяет транспорт
+    частная внутренняя скорость;
+    
+
+    public int getVolumeLine() {
+        internal combustion engine;
+
+        общественный пустой setVolumeLine(int VolumeLine) {
+            это. VolumeLine = volumeline;
+        }
+    
+        @Перекрыть
+        пустотная нагрузка() {
+            Система. вон. println("спортивные машины едут быстро на скорости");
+        }
+    
+    } 
